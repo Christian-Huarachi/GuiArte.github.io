b95c4eec8cc9c683927cea03ebaa1e6af9440f0e
Christian-Huarachi <chuarachivillca@frba.utn.edu.ar>
2023-10-26 12:32:12 -0300

    nuevo_branch

---
+++ guia.js
@@ -0,0 +1,109 @@
+const componente1 = {
+    template:`
+    <div style="display: flex;flex-direction: column;">
+        <h3>Guía Marvel's Spider-Man 2: Trucos, consejos y secretos</h3><br>
+        <p>Esto es todo lo que necesitas saber para completar la nueva aventura de Peter y Miles en Marvel's Spider-Man 2. Todos los trajes, misiones secundarias, coleccionables, easter eggs y mucho más.
+
+        </p><br>
+        <img src="recursos/spider.jpg"><br>
+        <h3>INICIO DE LA GUÍA</h3><br>
+        <p>Bienvenidos a nuestra guía completa de Marvel's Spider-Man 2. La nueva aventura de Peter Parker y Miles Morales en Nueva York nos vuelve a dejar a los mandos de los trepamuros, esta vez en equipo y por partida doble. A lo largo de esta guía os iremos explicando cómo completar todas las misiones evitando spoilers, así como lo que tenéis que hacer para completar las actividades secundarias y todos los coleccionables, sin olvidar las acciones individuales que hacen flata para algunos trofeos y el Platino. No te dejarás nada con nuestra guía, válida para conseguir el 100% sin problemas.
+        </p><br>
+        <h3>Historia y misiones principales</h3><br>
+        <img src="recursos/spidermision.jpg"><br>
+        <p>Marvel's Spider-Man 2 cuenta con un total de 31 misiones principales que debes superar para completar toda la historia de Peter y Miles. Han pasado 2 años desde la primera entrega y ambos han seguido con sus vidas. En esta sección de la guía podrás ver cómo superar cada obstáculo, además de cuándo están disponibles los contenidos secundarios. Todo ello, por supuesto, sin spoilers argumentales y evitando estropear ninguna sorpresa
+        </p><br>
+        <h3>Misiones</h3><br>
+        <img src="recursos/spider2.jpg"><br>
+        <p>La sección de misiones incluye las misiones principales y otras 10 cadenas de actividades o misiones secundarias. En esta sección verás cuándo y cómo puedes completar estas misiones, además de las recompensas en forma de materiales o trajes exclusivos que obtendrás por hacerlo.
+        </p><br>
+        <ul>
+            <li>Solicitudes de TAYVSM.</li>
+            <li>Alijos de Prowler.</li>
+            <li>Brooklyn Visions</li>
+            <li>La Llama.</li>
+            <li>Experimentos de la EMF.</li>
+            <li>Escondites de los Cazadores.</li>
+            <li>Bases de los Cazadores.</li>
+            <li>Museo de cultura.</li>
+            <li>Objetivos no identificados.</li>
+            <li>Nidos de simbiontes.</li>
+        </ul>
+        
+    </div>
+    `
+}
+const componente2 = {
+    // Datos de la receta
+    template: `
+    <div style="display: flex;flex-direction: column;">
+        <h3>Guía Marvel's Spider-Man 2: Trucos, consejos y secretos</h3>
+        <p>Esto es todo lo que necesitas saber para completar la nueva aventura de Peter y Miles en Marvel's Spider-Man 2. Todos los trajes, misiones secundarias, coleccionables, easter eggs y mucho más.
+
+        </p>
+        <img src="recursos/spider.jpg">
+    </div>
+    `
+}
+const componente3 = {
+    // Datos de la cocina
+    template: `
+    <div class="container"><br>
+        <b>La cocina</b>
+    </div>
+    <div class="container2">
+    <p>La cocina es el sitio en el cual se prepara la comida.
+       Puede ser el ambiente dedicado a esa tarea en un hogar 
+       o el espacio específico en un restaurante. 
+       En la cocina pueden lavarse, cortarse, hervirse, asarse,
+       fritarse o guisarse los alimentos.</p>
+
+    <div class="container">
+       <img src="img/cocina.jpg" alt="cocina por dentro" style=" width: 85%; max-width: 500px;">
+    </div>
+    <p>El diseño de una cocina de restaurante (más en general
+    una cocina profesional) es el resultado de un trabajo en
+    equipo, en donde las personas que lo dirigen se involucran
+    en la organización de los espacios y de las funciones.</p>
+     </div>
+    `
+}
+const componente4 = {
+    // Quienes somos
+    template: `
+    <div class="container"><br>
+        <b>¿Quienes somos?</b>
+    </div>
+    <div class="container2">
+    <p>La Cocina de Juan lleva más de 12 años cocinando y
+      distribuyendo comida elaborada en una ciudad de 28 mil
+      habitantes</p>
+
+    <div class="container">
+       <img src="img/nosotros1.jpg" alt="cocineros" style=" width: 85%; max-width: 500px;">
+    </div>
+    <p>Usamos productos de primera calidad y nuestras porciones
+     son abundantes.</p>
+    </div>
+    `
+}
+const componente9 = {
+    // Footer
+    template: `
+    <div class="container">
+    <hr>
+    <a  href="https://www.twitter.com" target="_blank"><i class="glowon fa fa-twitter fa-2x"
+                    aria-hidden="true"></i></a>&nbsp;&nbsp;
+            <a  href="https://www.facebook.com" target="_blank"> <i class="glowon fa fa-facebook fa-2x"
+                    aria-hidden="true"></i></a>&nbsp;&nbsp;
+            <a href="https://www.pinterest" target="_blank"> <i class="glowon fa fa-pinterest fa-2x"
+                    aria-hidden="true"></i></a>&nbsp;&nbsp;
+            <a href="https://www.instagram.com/hobbystarg/" target="_blank"><i class="glowon fa fa-instagram fa-2x"
+                    aria-hidden="true"></i></a>&nbsp;&nbsp;
+            <a href="https://www.linkedin.com/in/prof-ariel-palazzesi/" target="_blank"><i class="glowon fa fa-linkedin fa-2x"
+                    aria-hidden="true"></i> </a>
+            <p><b>Codo a Codo 2023</b></p>
+            <hr>
+    </div>
+    `
+}
--- script.js
+++ script.js
@@ -46,3 +46,63 @@ function validarEnviar(){
     }
 }
 
+
+
+
+    // Creamos nuestra aplicacion.
+    /*const app = Vue.createApp({
+        // DATOS de la aplicacion
+        data() {
+            // Definimos los valores que posee la aplicación
+            // (Son similares a las propiedades de un objeto)
+            return {
+                contenidoHTML:'<div class="img"><img src="recursos/img.jpg"></div><div class="descripcion"><h3>Nombre del juego</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed maxime, ex assumenda ea ipsum quasi provident dignissimos aspernatur iusto?</p></div>'
+            }
+        }
+    });
+
+    // Montamos la aplicacion en el div que tiene id="app":
+    app.mount('#app');*/
+
+
+
+
+// Creamos nuestra aplicacion.
+const miAplicacion = Vue.createApp({
+    components: {
+        'spider': componente1,
+        'juego2': componente2,
+        'cocina': componente3,
+        'sobre': componente4
+    },
+    data() {
+        return {
+            spider_visible: false,
+            juego2_visible: false,
+            cocina_visible: false,
+            sobre_visible: false
+        }
+    },
+    methods: {
+        show(componente) {
+            this.spider_visible = false
+            this.juego2_visible = false
+            this.cocina_visible = false
+            this.sobre_visible = false
+            console.clear()
+            console.log(componente)
+            if (componente == "spider") {
+                this.spider_visible = true
+            } else if (componente == "juego2") {
+                this.juego2_visible = true
+            } else if (componente == "cocina") {
+                this.cocina_visible = true
+            } else if (componente == "sobre") {
+                this.sobre_visible = true
+            }
+        }
+    }
+})
+
+
+miAplicacion.mount("#app")
